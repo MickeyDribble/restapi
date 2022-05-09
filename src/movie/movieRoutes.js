cc05200fe485e5add676a55dd3c9895ad660bd37
Michael <michaelf6988@gmail.com>
2022-05-09 17:24:12 +0100

    first

---
+++ src/movie/movieRoutes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+
+const { addMovie, listMovies, deleteMovie, updateMovie } = require("./movieControllers");
+
+const movieRouter = Router();
+
+
+//use http verb post to add data to our movie endpoint
+movieRouter.post("/movie", addMovie);
+movieRouter.get("/movie", listMovies);
+movieRouter.delete("/movie", deleteMovie);
+movieRouter.patch("/movie", updateMovie);
+
+module.exports = movieRouter;
