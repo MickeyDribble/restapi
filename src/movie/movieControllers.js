89b0b57436e3d1def69028f66ae37a4c54b5c241
Michael <michaelf6988@gmail.com>
2022-05-10 11:55:22 +0100

    second

--- src/movie/movieControllers.js
+++ src/movie/movieControllers.js
@@ -1,43 +1,44 @@
 const Movie = require("./movieModel");
 
 exports.addMovie = async (req, res) => {
-    try {
-       const newMovie = await Movie.create(req.body);
-       res.status(200).send({ movie: newMovie})
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ error: error.message})
-    }
+  try {
+    const newMovie = await Movie.create(req.body);
+    res.status(200).send({ movie: newMovie });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
 };
 
 exports.deleteMovie = async (req, res) => {
-    try {
-       const newMovie = await Movie.deleteOne(req.body);
-       res.status(200).send({ movie: newMovie})
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ error: error.message})
-    }
+  try {
+    const newMovie = await Movie.deleteOne(req.body);
+    res.status(200).send({ movie: newMovie });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
 };
 
 exports.updateMovie = async (req, res) => {
-    try {
-       const newMovie = await Movie.updateOne(req.body);
-       res.status(200).send({ movie: newMovie})
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ error: error.message})
-    }
+  try {
+    const newMovie = await Movie.updateOne(
+      { title: req.body.title},
+      { $set: { actors: req.body.actors } }
+    );
+    res.status(200).send({ movie: newMovie });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
 };
 
-
-
-exports.listMovies = async (req,res) => {
-    try {
-       const movies = await Movie.find({});
-       res.status(200).send({ movies});
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({error: error.message})
-    }
+exports.listMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find({});
+    res.status(200).send({ movies });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
 };
